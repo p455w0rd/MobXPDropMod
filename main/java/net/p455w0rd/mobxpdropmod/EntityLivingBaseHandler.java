6650297cde03780fbecc1c1a2bab358469b3db0b
p455w0rd <mike@p455w0rd.net>
2015-05-08 05:59:35 -0400

    Added reflection
    Added reflection so it works when launching from Eclipse and when
    launching from launcher...

--- main/java/net/p455w0rd/mobxpdropmod/EntityLivingBaseHandler.java
+++ main/java/net/p455w0rd/mobxpdropmod/EntityLivingBaseHandler.java
@@ -2,46 +2,36 @@ package net.p455w0rd.mobxpdropmod;
 
 import java.lang.reflect.Method;
 
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.FakePlayerFactory;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class EntityLivingBaseHandler {
-	public static final Method xpPoints = getExperiencePoints();
-
 	@SubscribeEvent
 	public void dropEvent(LivingDropsEvent event){
 		EntityLivingBase entity = event.entityLiving;
 		World world = entity.worldObj;
 		if(entity != null){
-			EntityLivingBase living = entity;
-			if(!world.isRemote && !event.recentlyHit){
-				int xp = 0;
+			EntityLivingBase elb = entity;
+			if(!world.isRemote && !event.recentlyHit && world.getGameRules().getGameRuleBooleanValue("doMobLoot")){
+				int i = 0;
 				try{
-					xp = (Integer)xpPoints.invoke(living, FakePlayerFactory.getMinecraft((WorldServer)world));
-				}catch(Exception ex){ throw new RuntimeException(ex); }
-				while(xp > 0){
-					int cap = EntityXPOrb.getXPSplit(xp);
-					xp -= cap;
-					living.worldObj.spawnEntityInWorld(new EntityXPOrb(living.worldObj, living.posX, living.posY, living.posZ, cap));
+					Method m = ReflectionHelper.findMethod(EntityLivingBase.class, elb, new String[] {"func_70693_a", "getExperiencePoints"}, EntityPlayer.class);
+					m.setAccessible(true);
+					i = (Integer)m.invoke(elb, FakePlayerFactory.getMinecraft((WorldServer)world));
+				}catch(Exception e){ }
+				while(i > 0){
+					int cap = EntityXPOrb.getXPSplit(i);
+					i -= cap;
+					elb.worldObj.spawnEntityInWorld(new EntityXPOrb(elb.worldObj, elb.posX, elb.posY, elb.posZ, cap));
 				}
 			}
 		}
-	}
-
-	public static Method getExperiencePoints(){
-		Method e = null;
-		try {
-			e = EntityLivingBase.class.getDeclaredMethod("func_70693_a", EntityPlayer.class);
-			e.setAccessible(true);
-		} catch(Exception ex) { throw new RuntimeException(ex); }
-		return e;
 	}
 }
